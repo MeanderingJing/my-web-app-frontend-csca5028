5be8a9cf0eb8aab580cfc270cfc78c1450e7e96d
pa-test <emma.lijing0723@gmail.com>
2025-07-16 09:49:39 -0700

    Initial commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    API_URL: process.env.API_URL || 'https://my-flask-app-csca5028-jing-ac19ad42b89b.herokuapp.com/',
+  },
+  // Import for Heroku deployment
+  output: 'standalone',
 };
 
 export default nextConfig;
